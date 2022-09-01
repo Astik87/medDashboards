051b7abd4aafcda3ca2d49bd3148024fc72bb5f1
Astik87 <astiksheriev@yandex.ru>
2022-09-01 10:20:03 +0300

    fix components

---
+++ client/src/styles/Button.js
@@ -0,0 +1,14 @@
+const sendBtnStyle = {
+    backgroundColor: '#3361FF',
+    fontWeight: 600,
+    padding: 10
+}
+
+const addBtnStyle = {
+    backgroundColor: '#3361FF',
+    marginRight: 10,
+    color: '#fff',
+    fontSize: 12,
+}
+
+export {sendBtnStyle, addBtnStyle}
---
+++ client/src/styles/Modal.js
@@ -0,0 +1,12 @@
+const modalBoxStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    p: 2,
+    maxWidth: '95vw'
+}
+
+export {modalBoxStyle}
